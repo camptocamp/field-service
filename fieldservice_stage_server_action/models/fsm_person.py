d102f7c43b34ecc352683b6f1d4763accbe87fde
BojanOD <bojanancev@gmail.com>
2022-12-12 21:29:27 +0100

    [14.0][IMP] Fieldservice Stage Server Action

---
+++ fieldservice_stage_server_action/models/fsm_person.py
@@ -0,0 +1,31 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class FSMPerson(models.Model):
+    _inherit = "fsm.person"
+
+    @api.model
+    def create(self, vals):
+        res = super(FSMPerson, self).create(vals)
+        action = self.env["fsm.stage"].browse(vals.get("stage_id")).action_id
+        if action:
+            context = {
+                "active_model": self._name,
+                "active_ids": [res.id],
+            }
+            action.with_context(**context).run()
+        return res
+
+    def write(self, vals):
+        res = super().write(vals)
+        action = self.env["fsm.stage"].browse(vals.get("stage_id")).action_id
+        if action:
+            context = {
+                "active_model": self._name,
+                "active_ids": self.ids,
+            }
+            action.with_context(**context).run()
+        return res
