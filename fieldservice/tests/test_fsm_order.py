ecf3c8d9c3a659c8156bd11037777253d5c9e14a
Iván Todorovich <ivan.todorovich@gmail.com>
2024-12-03 11:29:28 -0300

    [IMP] fieldservice: use computed fields instead of onchanges

--- fieldservice/tests/test_fsm_order.py
+++ fieldservice/tests/test_fsm_order.py
@@ -172,7 +172,6 @@ class TestFSMOrder(TransactionCase):
         order4.action_complete()
         order3.action_cancel()
         self.env.user.company_id.auto_populate_equipments_on_order = True
-        order._onchange_location_id_customer()
         self.assertEqual(order.custom_color, order.stage_id.custom_color)
         # Test _compute_duration
         self.assertEqual(order.duration, hours_diff)
@@ -238,18 +237,11 @@ class TestFSMOrder(TransactionCase):
                     "sequence": 10,
                 }
             )
-        order.description = "description"
-        order.copy_notes()
-        order.description = False
-        order.copy_notes()
-        order.type = False
         order.equipment_id = equipment.id
-        order.onchange_equipment_ids()
         order.type = False
         order.description = False
         self.location_1.direction = "Test Direction"
         order2.location_id.fsm_parent_id = self.location_1.id
-        order.copy_notes()
         data = (
             self.env["fsm.order"]
             .with_context(**{"default_team_id": self.test_team.id})
